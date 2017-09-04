47eec48c726b39f1563d04dcb3b678c6a80cc7ee
Quynh Nguyen <mr.quynh@gmail.com>
2017-09-05 01:44:30 +0700

    Update name

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following set of attributes.
 // Change these attribute values to modify the information associated with an assembly.
-[assembly: AssemblyTitle("VIEApps NGX Service Base")]
+[assembly: AssemblyTitle("VIEApps NGX")]
 [assembly: AssemblyDescription("Core components of VIEApps NGX")]
 [assembly: AssemblyDefaultAlias("VIEApps.Services.Base")]
 [assembly: AssemblyConfiguration("Production/Stable")]
