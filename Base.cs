79653a134df17d0c7417400f93a77fe28ad31c84
Quynh Nguyen <mr.quynh@gmail.com>
2017-09-15 02:44:01 +0700

    Improvement: inter-communicate messages

--- Base.cs
+++ Base.cs
@@ -257,17 +257,11 @@ namespace net.vieapps.Services
 
 				// register the handler of inter-communicate messages
 				this._communicator?.Dispose();
-				this._communicator = this._incommingChannel.RealmProxy.Services.GetSubject<CommunicateMessage>("net.vieapps.rtu.communicate.messages")
+				this._communicator = this._incommingChannel.RealmProxy.Services
+					.GetSubject<CommunicateMessage>("net.vieapps.rtu.communicate.messages." + this.ServiceName.Trim().ToLower())
 					.Subscribe<CommunicateMessage>(
-						(message) =>
-						{
-							if (message.ServiceName.IsEquals(this.ServiceName))
-								this.ProcessInterCommunicateMessage(message);
-						},
-						(exception) =>
-						{
-							this.WriteLog(UtilityService.BlankUID, "APIGateway", "RTU", "Error occurred while fetching inter-communicate message", exception);
-						}
+						message => this.ProcessInterCommunicateMessage(message),
+						exception => this.WriteLog(UtilityService.BlankUID, "APIGateway", "RTU", "Error occurred while fetching inter-communicate message", exception)
 					);
 
 				// callback when done
