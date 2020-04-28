13a279c8cd69ba585c3fcbd38d34564cb2d3c43b
Quynh Nguyen <mr.quynh@gmail.com>
2020-04-28 22:28:16 +0700

    (improv): compute node identity

--- Extensions.Environment.cs
+++ Extensions.Environment.cs
@@ -45,33 +45,57 @@ namespace net.vieapps.Services
 			=> $"- User: {Environment.UserName.ToLower()} @ {Environment.MachineName.ToLower()}{seperator ?? "\r\n\t"}- Platform: {Extensions.GetRuntimePlatform()}";
 		#endregion
 
-		#region Get unique name & end-point
+		#region Get node identity, unique name & end-point
 		/// <summary>
-		/// Gets the unique name of a business service
+		/// Gets the identity of a node that running a service
 		/// </summary>
-		/// <param name="name">The string that presents name of a business service</param>
-		/// <param name="user">The string that presents name of the user who runs the process of the business service</param>
-		/// <param name="host">The string that presents name of the host that runs the process of the business service</param>
-		/// <param name="platform">The string that presents name of the platform that runs the process of the business service</param>
-		/// <param name="os">The string that presents name of the operating system that runs the process of the business service</param>
-		/// <returns>The string that presents unique name of a business service at a host</returns>
-		public static string GetUniqueName(string name, string user = null, string host = null, string platform = null, string os = null)
-			=> $"{(name ?? "unknown").Trim().ToLower()}.{(user ?? Environment.UserName).Trim().ToLower()}-{(host ?? Environment.MachineName).Trim().ToLower()}-" + $"{(platform ?? RuntimeInformation.FrameworkDescription).Trim()} @ {os ?? Extensions.GetRuntimeOS()}".GenerateUUID();
+		/// <param name="user">The user on the host that running the service</param>
+		/// <param name="host">The host that running the service</param>
+		/// <param name="platform">The information (description) of the running platform (framework)</param>
+		/// <param name="os">The information of the operating system</param>
+		/// <returns>The string that presents the identity of a node (include user and host)</returns>
+		public static string GetNodeID(string user = null, string host = null, string platform = null, string os = null)
+		{
+			user = (user ?? Environment.UserName).Trim().ToLower();
+			host = (host ?? Environment.MachineName).Trim().ToLower();
+			platform = (platform ?? RuntimeInformation.FrameworkDescription).Trim().ToLower();
+			os = (os ?? Extensions.GetRuntimeOS()).Trim().ToLower();
+			return $"{user}-{host}-" + $"{platform} @ {os}".GenerateUUID();
+		}
+
+		/// <summary>
+		/// Gets the identity of a node that running a service
+		/// </summary>
+		/// <param name="args">The running (starting) arguments</param>
+		/// <returns>The string that presents the identity of a node (include user and host)</returns>
+		public static string GetNodeID(IEnumerable<string> args)
+			=> Extensions.GetNodeID(
+				args?.FirstOrDefault(arg => arg.IsStartsWith("/run-user:"))?.Replace(StringComparison.OrdinalIgnoreCase, "/run-user:", "").UrlDecode(),
+				args?.FirstOrDefault(arg => arg.IsStartsWith("/run-host:"))?.Replace(StringComparison.OrdinalIgnoreCase, "/run-host:", "").UrlDecode(),
+				args?.FirstOrDefault(arg => arg.IsStartsWith("/run-platform:"))?.Replace(StringComparison.OrdinalIgnoreCase, "/run-platform:", "").UrlDecode(),
+				args?.FirstOrDefault(arg => arg.IsStartsWith("/run-os:"))?.Replace(StringComparison.OrdinalIgnoreCase, "/run-os:", "").UrlDecode()
+			);
 
 		/// <summary>
 		/// Gets the unique name of a business service
 		/// </summary>
-		/// <param name="name">The string that presents name of a business service</param>
-		/// <param name="args">The starting arguments</param>
+		/// <param name="name">The string that presents the name of a service</param>
+		/// <param name="user">The user on the host that running the service</param>
+		/// <param name="host">The host that running the service</param>
+		/// <param name="platform">The information (description) of the running platform (framework)</param>
+		/// <param name="os">The information of the operating system</param>
 		/// <returns>The string that presents unique name of a business service at a host</returns>
-		public static string GetUniqueName(string name, string[] args)
-		{
-			var user = args?.FirstOrDefault(a => a.IsStartsWith("/run-user:"));
-			var host = args?.FirstOrDefault(a => a.IsStartsWith("/run-host:"));
-			var platform = args?.FirstOrDefault(a => a.IsStartsWith("/run-platform:"));
-			var os = args?.FirstOrDefault(a => a.IsStartsWith("/run-os:"));
-			return Extensions.GetUniqueName(name, user?.Replace(StringComparison.OrdinalIgnoreCase, "/run-user:", "").Trim().UrlDecode(), host?.Replace(StringComparison.OrdinalIgnoreCase, "/run-host:", "").Trim().UrlDecode(), platform?.Replace(StringComparison.OrdinalIgnoreCase, "/run-platform:", "").Trim().UrlDecode(), os?.Replace(StringComparison.OrdinalIgnoreCase, "/run-os:", "").Trim().UrlDecode());
-		}
+		public static string GetUniqueName(string name, string user = null, string host = null, string platform = null, string os = null)
+			=> $"{(name ?? "unknown").Trim().ToLower()}.{Extensions.GetNodeID(user, host, platform, os)}";
+
+		/// <summary>
+		/// Gets the unique name of a business service
+		/// </summary>
+		/// <param name="name">The string that presents the name of a service</param>
+		/// <param name="args">The running (starting) arguments</param>
+		/// <returns>The string that presents unique name of a service</returns>
+		public static string GetUniqueName(string name, IEnumerable<string> args)
+			=> $"{(name ?? "unknown").Trim().ToLower()}.{Extensions.GetNodeID(args)}";
 
 		/// <summary>
 		/// Gets the resolved URI with IP address and port
@@ -103,38 +127,33 @@ namespace net.vieapps.Services
 		/// <param name="available">The available state</param>
 		/// <param name="cancellationToken">The cancellation token</param>
 		/// <returns></returns>
-		public static Task SendServiceInfoAsync(this IRTUService rtuService, string serviceName, string[] args, bool running, bool available = true, CancellationToken cancellationToken = default)
+		public static Task SendServiceInfoAsync(this IRTUService rtuService, string serviceName, IEnumerable<string> args, bool running, bool available = true, CancellationToken cancellationToken = default)
 		{
+			if (rtuService == null || string.IsNullOrWhiteSpace(serviceName))
+				return Task.CompletedTask;
+
 			var arguments = (args ?? new string[] { }).Where(arg => !arg.IsStartsWith("/controller-id:")).ToArray();
-			var invokeInfo = arguments.FirstOrDefault(arg => arg.IsStartsWith("/call-user:")) ?? "";
-
-			if (!string.IsNullOrWhiteSpace(invokeInfo))
+			var invokeInfo = (arguments.FirstOrDefault(arg => arg.IsStartsWith("/call-user:"))?.Replace(StringComparison.OrdinalIgnoreCase, "/call-user:", "").UrlDecode().Trim().ToLower() ?? Environment.UserName.ToLower())
+				+ " [Host: "
+				+ (arguments.FirstOrDefault(arg => arg.IsStartsWith("/call-host:"))?.Replace(StringComparison.OrdinalIgnoreCase, "/call-host:", "").UrlDecode().Trim().ToLower() ?? Environment.MachineName.ToLower())
+				+ " - Platform: "
+				+ (arguments.FirstOrDefault(arg => arg.IsStartsWith("/call-platform:"))?.Replace(StringComparison.OrdinalIgnoreCase, "/call-platform:", "").UrlDecode() ?? RuntimeInformation.FrameworkDescription.Trim())
+				+ " @ "
+				+ (arguments.FirstOrDefault(arg => arg.IsStartsWith("/call-os:"))?.Replace(StringComparison.OrdinalIgnoreCase, "/call-os:", "").UrlDecode() ?? Extensions.GetRuntimePlatform(false))
+				+ "]";
+			return rtuService.SendInterCommunicateMessageAsync(new CommunicateMessage("APIGateway")
 			{
-				invokeInfo = invokeInfo.Replace(StringComparison.OrdinalIgnoreCase, "/call-user:", "").UrlDecode();
-				var host = arguments.FirstOrDefault(arg => arg.IsStartsWith("/call-host:"));
-				var platform = arguments.FirstOrDefault(arg => arg.IsStartsWith("/call-platform:"));
-				var os = arguments.FirstOrDefault(arg => arg.IsStartsWith("/call-os:"));
-				if (!string.IsNullOrWhiteSpace(host) && !string.IsNullOrWhiteSpace(platform) && !string.IsNullOrWhiteSpace(os))
-					invokeInfo += $" [Host: {host.Replace(StringComparison.OrdinalIgnoreCase, "/call-host:", "").UrlDecode()} - Platform: {platform.Replace(StringComparison.OrdinalIgnoreCase, "/call-platform:", "").UrlDecode()} @ {os.Replace(StringComparison.OrdinalIgnoreCase, "/call-os:", "").UrlDecode()}]";
-			}
-			else
-				invokeInfo = $"{Environment.UserName.ToLower()} [Host: {Environment.MachineName.ToLower()} - Platform: {Extensions.GetRuntimePlatform()}]";
-
-			return rtuService == null || string.IsNullOrWhiteSpace(serviceName)
-				? Task.CompletedTask
-				: rtuService.SendInterCommunicateMessageAsync(new CommunicateMessage("APIGateway")
+				Type = "Service#Info",
+				Data = new ServiceInfo
 				{
-					Type = "Service#Info",
-					Data = new ServiceInfo
-					{
-						Name = serviceName.ToLower(),
-						UniqueName = Extensions.GetUniqueName(serviceName, arguments),
-						ControllerID = args?.FirstOrDefault(arg => arg.IsStartsWith("/controller-id:"))?.Replace("/controller-id:", "") ?? "Unknown",
-						InvokeInfo = invokeInfo,
-						Available = available,
-						Running = running
-					}.ToJson()
-				}, cancellationToken);
+					Name = serviceName.ToLower(),
+					UniqueName = Extensions.GetUniqueName(serviceName, arguments),
+					ControllerID = args?.FirstOrDefault(arg => arg.IsStartsWith("/controller-id:"))?.Replace("/controller-id:", "") ?? "Unknown",
+					InvokeInfo = invokeInfo,
+					Available = available,
+					Running = running
+				}.ToJson()
+			}, cancellationToken);
 		}
 		#endregion
 
