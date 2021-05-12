743162a3dfb8030ad58f167f6ff5b2b017588a06
Quynh Nguyen <mr.quynh@gmail.com>
2021-05-13 00:24:05 +0700

    (improv): try/catch when send messages

--- Extensions.RTU.cs
+++ Extensions.RTU.cs
@@ -31,7 +31,13 @@ namespace net.vieapps.Services
 		/// <param name="message"></param>
 		/// <returns></returns>
 		public static void Send(this UpdateMessage message)
-			=> Extensions.GetUpdatingSubject()?.OnNext(message);
+		{
+			try
+			{
+				Extensions.GetUpdatingSubject()?.OnNext(message);
+			}
+			catch { }
+		}
 
 		/// <summary>
 		/// Sends an updating message
@@ -41,15 +47,8 @@ namespace net.vieapps.Services
 		/// <returns></returns>
 		public static Task SendAsync(this UpdateMessage message, CancellationToken cancellationToken = default)
 		{
-			try
-			{
-				message?.Send();
-				return Task.CompletedTask;
-			}
-			catch (Exception ex)
-			{
-				return Task.FromException(ex);
-			}
+			message?.Send();
+			return Task.CompletedTask;
 		}
 
 		/// <summary>
@@ -62,7 +61,14 @@ namespace net.vieapps.Services
 			if (messages != null && messages.Any())
 			{
 				var subject = Extensions.GetUpdatingSubject();
-				messages.ForEach(message => subject?.OnNext(message));
+				messages.ForEach(message =>
+				{
+					try
+					{
+						subject?.OnNext(message);
+					}
+					catch { }
+				});
 			}
 		}
 
@@ -74,15 +80,8 @@ namespace net.vieapps.Services
 		/// <returns></returns>
 		public static Task SendAsync(this IEnumerable<UpdateMessage> messages, CancellationToken cancellationToken = default)
 		{
-			try
-			{
-				messages?.Send();
-				return Task.CompletedTask;
-			}
-			catch (Exception ex)
-			{
-				return Task.FromException(ex);
-			}
+			messages?.Send();
+			return Task.CompletedTask;
 		}
 
 		/// <summary>
@@ -93,13 +92,19 @@ namespace net.vieapps.Services
 		/// <param name="excludedDeviceID"></param>
 		/// <returns></returns>
 		public static void Send(this List<BaseMessage> messages, string deviceID, string excludedDeviceID)
-			=> messages?.Select(message => new UpdateMessage
+		{
+			try
 			{
-				Type = message.Type,
-				Data = message.Data,
-				DeviceID = deviceID,
-				ExcludedDeviceID = excludedDeviceID
-			})?.Send();
+				messages?.Select(message => new UpdateMessage
+				{
+					Type = message.Type,
+					Data = message.Data,
+					DeviceID = deviceID,
+					ExcludedDeviceID = excludedDeviceID
+				})?.Send();
+			}
+			catch { }
+		}
 
 		/// <summary>
 		/// Sends the collection of updating messages
@@ -140,7 +145,13 @@ namespace net.vieapps.Services
 		/// <param name="message"></param>
 		/// <returns></returns>
 		public static void Send(this CommunicateMessage message)
-			=> message?.GetCommunicatingSubject()?.OnNext(message);
+		{
+			try
+			{
+				message?.GetCommunicatingSubject()?.OnNext(message);
+			}
+			catch { }
+		}
 
 		/// <summary>
 		/// Sends a communicating message
@@ -150,15 +161,8 @@ namespace net.vieapps.Services
 		/// <returns></returns>
 		public static Task SendAsync(this CommunicateMessage message, CancellationToken cancellationToken = default)
 		{
-			try
-			{
-				message?.Send();
-				return Task.CompletedTask;
-			}
-			catch (Exception ex)
-			{
-				return Task.FromException(ex);
-			}
+			message?.Send();
+			return Task.CompletedTask;
 		}
 
 		/// <summary>
@@ -171,7 +175,14 @@ namespace net.vieapps.Services
 			if (messages != null && messages.Any())
 			{
 				var subject = messages.First().GetCommunicatingSubject();
-				messages.ForEach(message => subject?.OnNext(message));
+				messages.ForEach(message =>
+				{
+					try
+					{
+						subject?.OnNext(message);
+					}
+					catch { }
+				});
 			}
 		}
 
@@ -183,15 +194,8 @@ namespace net.vieapps.Services
 		/// <returns></returns>
 		public static Task SendAsync(this IEnumerable<CommunicateMessage> messages, CancellationToken cancellationToken = default)
 		{
-			try
-			{
-				messages?.Send();
-				return Task.CompletedTask;
-			}
-			catch (Exception ex)
-			{
-				return Task.FromException(ex);
-			}
+			messages?.Send();
+			return Task.CompletedTask;
 		}
 
 		/// <summary>
@@ -205,7 +209,14 @@ namespace net.vieapps.Services
 			if (messages != null && messages.Any())
 			{
 				var subject = messages.First().GetCommunicatingSubject($"messages.services.{serviceName.Trim().ToLower()}");
-				messages.Select(message => new CommunicateMessage(serviceName, message)).ForEach(message => subject?.OnNext(message));
+				messages.Select(message => new CommunicateMessage(serviceName, message)).ForEach(message =>
+				{
+					try
+					{
+						subject?.OnNext(message);
+					}
+					catch { }
+				});
 			}
 		}
 
@@ -218,102 +229,8 @@ namespace net.vieapps.Services
 		/// <returns></returns>
 		public static Task SendAsync(this List<BaseMessage> messages, string serviceName, CancellationToken cancellationToken = default)
 		{
-			try
-			{
-				messages?.Send(serviceName);
-				return Task.CompletedTask;
-			}
-			catch (Exception ex)
-			{
-				return Task.FromException(ex);
-			}
-		}
-
-		static string LogsPath = UtilityService.GetAppSetting("Path:Logs", "logs");
-
-		/// <summary>
-		/// Writes the log messages into centerlized log storage
-		/// </summary>
-		/// <param name="loggingService"></param>
-		/// <param name="logs"></param>
-		/// <param name="sessionBuilder"></param>
-		/// <param name="logger"></param>
-		/// <param name="cancellationToken"></param>
-		/// <returns></returns>
-		public static async Task WriteLogsAsync(this ILoggingService loggingService, ConcurrentQueue<Tuple<Tuple<DateTime, string, string, string, string, string>, List<string>, string>> logs, Func<Session> sessionBuilder = null, ILogger logger = null, CancellationToken cancellationToken = default)
-		{
-			Tuple<Tuple<DateTime, string, string, string, string, string>, List<string>, string> log = null;
-			try
-			{
-				while (logs.TryDequeue(out log))
-					await loggingService.WriteLogsAsync(log.Item1.Item2, log.Item1.Item3, log.Item1.Item4, log.Item1.Item5, log.Item1.Item6, log.Item2, log.Item3, cancellationToken).ConfigureAwait(false);
-			}
-			catch (Exception ex)
-			{
-				if (log != null)
-					logs.Enqueue(log);
-
-				if (ex is WampException && ex.Message.IsEquals("wamp.error.no_such_procedure"))
-				{
-					var sessionID = UtilityService.NewUUID;
-					var session = sessionBuilder?.Invoke() ?? new Session
-					{
-						SessionID = sessionID,
-						User = new User
-						{
-							SessionID = sessionID,
-							ID = UtilityService.GetAppSetting("Users:SystemAccountID", "VIEAppsNGX-MMXVII-System-Account")
-						},
-						IP = "127.0.0.1",
-						DeviceID = $"{ServiceBase.ServiceComponent?.NodeID ?? UtilityService.NewUUID}@logger",
-						AppName = "VIEApps NGX Logger",
-						AppPlatform = $"{Extensions.GetRuntimeOS()} Daemon",
-						AppAgent = $"{UtilityService.DesktopUserAgent} VIEApps NGX Daemon/{typeof(ServiceBase).Assembly.GetVersion(false)}",
-						AppOrigin = null,
-						Verified = true
-					};
-
-					var flushIntoFiles = false;
-					while (logs.TryDequeue(out log))
-						try
-						{
-							var json = new JObject
-							{
-								{ "Time", log.Item1.Item1 },
-								{ "CorrelationID", log.Item1.Item2 },
-								{ "DeveloperID", log.Item1.Item3 },
-								{ "AppID", log.Item1.Item4 },
-								{ "ServiceName", log.Item1.Item5 },
-								{ "ObjectName", log.Item1.Item6 },
-								{ "Logs", log.Item2?.Join("\r\n") ?? "" },
-								{ "Stack", log.Item3 }
-							};
-							if (flushIntoFiles)
-								try
-								{
-									var filePath = Path.Combine(Extensions.LogsPath, $"logs.services.{DateTime.Now:yyyyMMddHHmmss}.{UtilityService.NewUUID}.json");
-									await UtilityService.WriteTextFileAsync(filePath, json.ToString(Formatting.Indented), false, null, cancellationToken).ConfigureAwait(false);
-								}
-								catch { }
-							else
-								await Router.CallServiceAsync(new RequestInfo
-								{
-									Session = session,
-									ServiceName = "logs",
-									ObjectName = "service",
-									Verb = "POST",
-									Body = json.ToString(Formatting.None),
-									CorrelationID = log.Item1.Item2
-								}, cancellationToken).ConfigureAwait(false);
-						}
-						catch
-						{
-							flushIntoFiles = true;
-							if (log != null)
-								logs.Enqueue(log);
-						}
-				}
-			}
+			messages?.Send(serviceName);
+			return Task.CompletedTask;
 		}
 	}
 }
